4a84919f3f95a608cb6703aa295679fe2acc9deb
Gad Lidror <gadl@tichonet.com>
2026-04-26 14:09:50 +0300

    xxxx

--- src/CampusIL/utils.py
+++ src/CampusIL/utils.py
@@ -1,5 +1,12 @@
 import numpy as np
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 import math
+>>>>>>> 95fd60708e045ca4de71da43c13ae344f9cc4d05
+=======
+import math
+>>>>>>> 95fd60708e045ca4de71da43c13ae344f9cc4d05
 
 def expect_assert(fn, *args):
   try:
@@ -8,8 +15,25 @@ def expect_assert(fn, *args):
     return True
   return False
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+def _close(a, b, tol=1e-6):
+    return abs(float(a) - float(b)) <= tol
+
+def _allclose(a, b, tol=1e-6):
+    a = np.array(a, dtype=float)
+    b = np.array(b, dtype=float)
+    return bool(np.all(np.abs(a-b) <= tol))
+=======
+=======
+>>>>>>> 95fd60708e045ca4de71da43c13ae344f9cc4d05
 def squarred_error(y_hat, y):
   return (y-y_hat)**2
 
 def abs_error(y_hat, y):
   return abs(y-y_hat)
+<<<<<<< HEAD
+>>>>>>> 95fd60708e045ca4de71da43c13ae344f9cc4d05
+=======
+>>>>>>> 95fd60708e045ca4de71da43c13ae344f9cc4d05
